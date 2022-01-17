d3c060c9b88d4534b0b9d38080cc88a19991884e
Jon Gentry <jonmarkgentry@gmail.com>
2022-01-17 14:29:06 -0800

    Add Check Permutation

---
+++ CTCI/01_Arrays_and_Strings/CheckPermutation/Sources/CheckPermutation/CheckPermutation.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  
+//
+//  Created by Jonathan Gentry on 1/17/22.
+//
+
+import Foundation
+
+func checkPermutation(str1: String, str2: String) -> Bool {
+  if str1.count != str2.count {
+    return false
+  }
+  return true
+}
